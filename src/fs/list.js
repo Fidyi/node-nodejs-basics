566fb8722e0b3097eb6abdf319ab54fc4eb4cc82
Sergei <106452081+Fidyi@users.noreply.github.com>
2024-10-04 11:57:25 +0300

    feat: fs-delete fs-list fs-read fs-rename

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const list = async () => {
-    // Write your code here 
+   const dirPath = path.join(__dirname, 'files');
+   try {
+        await fs.access(dirPath);
+   } catch(err) {
+    throw new Error('FS operation falied');
+   }
+   try {
+    const files = await fs.readdir(dirPath);
+    console.log(files)
+   } catch (err) {
+    console.error(err.message);
+    throw new Error('FS operation falied');
+   }
 };
-
 await list();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,22 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const read = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname,'files', 'fileToRead.txt');
+    try {
+        await fs.access(filePath);
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
+    try {
+        const content = await fs.readFile(filePath, 'utf-8');
+        console.log(content);
+    } catch (err) {
+        console.error(err.message);
+        throw new Error('FS operation failed');
+    }
 };
-
 await read();
