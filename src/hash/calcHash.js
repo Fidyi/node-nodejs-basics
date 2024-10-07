74dec1e56033231928f80c838a69291c0c9b9dec
Sergei <106452081+Fidyi@users.noreply.github.com>
2024-10-07 20:10:24 +0300

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import crypto from "crypto";
+import fs from "fs";
+import {fileURLToPath} from "url";
+import { dirname } from 'path';
+import path from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+    const hash = crypto.createHash('sha256');
+    const fileStream = fs.createReadStream(filePath);
+    fileStream.on("data", (chunk) => {
+        hash.update(chunk);
+    })
+    return new Promise((resolve, reject) => {
+        fileStream.on('end', () => {
+            const fileHash = hash.digest('hex');
+            console.log(`SHA256 hash: ${fileHash}`);
+            resolve(fileHash);
+        });
+        fileStream.on('error', (err) => {
+            console.error('Error reading the file', err);
+            reject(err);
+        });
+    });
 };
 
 await calculateHash();
